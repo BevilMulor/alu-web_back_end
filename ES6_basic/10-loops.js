f9540830621e4c99bb93e7fd9f914533892117d7
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2024-07-11 13:52:49 +0200

    es basics

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const array2 = array;
-    for (const value of array) {
-      const idx = array.indexOf(value);
-      array2[idx] = appendString + value;
-    }
-  
-    return array2;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}
